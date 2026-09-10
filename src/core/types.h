76fce56ffde581fe3cf7afd21cff49dc957b3d11
Kartikeya <kartikeya.anjul@gmail.com>
2026-09-10 22:23:39 +0530

    feat(Order type)

--- src/core/types.h
+++ src/core/types.h
@@ -13,5 +13,24 @@ namespace exchange::core{
     using SequenceNumber = std::uint64_t; // every state changing event in the system will get a monotonically increasing sequence number
     using Timestamp = std::uint64_t;
     using ParticipantId = std::uint32_t; //int assign at the login time
+    using MatchId = std::uint64_t; // it identify one trade that is one buyer seller paring
 
+    inline constexpr Price PRICE_SCALE = 10'000;
+
+    enum class Side : std::uint8_t{
+        BUY,
+        SELL,
+    };
+
+    enum class OrderType : std::uint8_t {
+        LIMIT,
+        MARKET,
+        IOC,
+        FOK,
+        GTC,
+        STOP,
+        STOP_LIMIT,
+        ICEBERG,
+        POST_ONLY,
+    };
 }
